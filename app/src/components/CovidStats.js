8a904e80d128cba77fc3227936010f63273b8a9c
Mary Baker <meb601@gmail.com>
2020-09-09 19:10:34 -0400

    Update map key in CovidStats

--- app/src/components/CovidStats.js
+++ app/src/components/CovidStats.js
@@ -6,9 +6,8 @@ function CovidStats(props) {
     return (
         <>
             <h3>Global Data</h3>
-            {/* <div>{props.stats[0].Country}</div> */}
             <div>{props.stats.map((stat) => {
-                return <Stat key={props.stats.Country} stat={stat} />;
+                return <Stat key={stat.Country} stat={stat} />;
             })}</div>
         </>
     );
