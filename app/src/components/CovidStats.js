485c078e4897daaba8be11c2296c789829193793
Mary Baker <meb601@gmail.com>
2020-09-10 15:14:23 -0400

    Add country search filter using useState

--- app/src/components/CovidStats.js
+++ app/src/components/CovidStats.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import Stat from "./Stat";
 
 function CovidStats(props) {
+
+    const [search, setSearch] = useState('')
+
+    const filteredCountries = props.stats.filter(country => {
+        return country.Country.toLowerCase().includes(search.toLowerCase())
+    })
+
     return (
         <>
             <h3>Global Data</h3>
-            <div>{props.stats.map((stat) => {
+            <forms>
+                <input type='text' placeholder='Search for a Country' onChange={e => setSearch(e.target.value)} />
+            </forms>
+            <div>{filteredCountries.map((stat) => {
                 return <Stat key={stat.Country} stat={stat} />;
             })}</div>
         </>
     );
+
 }
 
 function mapStateToProps(state) {
